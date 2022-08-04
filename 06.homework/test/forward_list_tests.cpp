349d928d586b26949341909020467ed7f05d3cba
EvgeniiaD <zhen_da@mail.ru>
2022-08-04 21:58:59 +0700

    Add new unit tests for containers.

--- 06.homework/test/forward_list_tests.cpp
+++ 06.homework/test/forward_list_tests.cpp
@@ -1,15 +1,15 @@
-#include "my_list.hpp"
+#include "my_forward_list.hpp"
 
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
 #include <algorithm>
-#include <list>
+#include <forward_list>
 
-TEST( list, empty )
+TEST( forward_list, empty )
 {
     // Arrange
-    MyList<int> list;
+    MyForwardList<int> list;
     
     // Act( empty for this test)
 
@@ -17,11 +17,11 @@ TEST( list, empty )
     ASSERT_EQ(list.size(), 0);
 }
 
-TEST( list, push_back )
+TEST( forward_list, push_back )
 {
     // Arrange
     const int count = 10;
-    MyList<int> list;
+    MyForwardList<int> list;
 
     // Act
     for( int i = 0; i < count; ++i )
@@ -31,14 +31,14 @@ TEST( list, push_back )
 
     // Assert
     ASSERT_EQ( list.size(), count );
-    std::list<int> list_expect = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    std::forward_list<int> list_expect = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     ASSERT_TRUE( std::equal( list_expect.begin(), list_expect.end(), list.begin() ) );
 }
 
-TEST( list, insert_back )
+TEST( forward_list, insert_back )
 {
     // Arrange
-    MyList<int> list;
+    MyForwardList<int> list;
     list.push_back( 0 );
     list.push_back( 1 );
 
@@ -47,14 +47,14 @@ TEST( list, insert_back )
 
     // Assert
     ASSERT_EQ( list.size(), 3 );
-    std::list<int> list_expect = {0, 1 , 2};
+    std::forward_list<int> list_expect = {0, 1 , 2};
     ASSERT_TRUE( std::equal( list_expect.begin(), list_expect.end(), list.begin() ) );
 }
 
-TEST( list, insert_front )
+TEST( forward_list, insert_front )
 {
     // Arrange
-    MyList<int> list;
+    MyForwardList<int> list;
     list.push_back( 0 );
     list.push_back( 1 );
 
@@ -63,14 +63,14 @@ TEST( list, insert_front )
 
     // Assert
     ASSERT_EQ( list.size(), 3 );
-    std::list<int> list_expect = {-1, 0 , 1};
+    std::forward_list<int> list_expect = {-1, 0 , 1};
     ASSERT_TRUE( std::equal( list_expect.begin(), list_expect.end(), list.begin() ) );
 }
 
-TEST( list, insert_midle )
+TEST( forward_list, insert_midle )
 {
     // Arrange
-    MyList<int> list;
+    MyForwardList<int> list;
     list.push_back( 0 );
     list.push_back( 1 );
 
@@ -79,14 +79,14 @@ TEST( list, insert_midle )
 
     // Assert
     ASSERT_EQ( list.size(), 3 );
-    std::list<int> list_expect = {0, 2 , 1};
+    std::forward_list<int> list_expect = {0, 2 , 1};
     ASSERT_TRUE( std::equal( list_expect.begin(), list_expect.end(), list.begin() ) );
 }
 
-TEST( list, erase_back )
+TEST( forward_list, erase_back )
 {
     // Arrange
-    MyList<int> list;
+    MyForwardList<int> list;
     list.push_back( 0 );
     list.push_back( 1 );
     list.push_back( 2 );
@@ -96,14 +96,14 @@ TEST( list, erase_back )
 
     // Assert
     ASSERT_EQ( list.size(), 2 );
-    std::list<int> list_expect = {0, 1};
+    std::forward_list<int> list_expect = {0, 1};
     ASSERT_TRUE( std::equal( list_expect.begin(), list_expect.end(), list.begin() ) );
 }
 
-TEST( list, erase_front )
+TEST( forward_list, erase_front )
 {
     // Arrange
-    MyList<int> list;
+    MyForwardList<int> list;
     list.push_back( 0 );
     list.push_back( 1 );
     list.push_back( 2 );
@@ -113,14 +113,14 @@ TEST( list, erase_front )
 
     // Assert
     ASSERT_EQ( list.size(), 2 );
-    std::list<int> list_expect = {1, 2};
+    std::forward_list<int> list_expect = {1, 2};
     ASSERT_TRUE( std::equal( list_expect.begin(), list_expect.end(), list.begin() ) );
 }
 
-TEST( list, erase_midle )
+TEST( forward_list, erase_midle )
 {
     // Arrange
-    MyList<int> list;
+    MyForwardList<int> list;
     list.push_back( 0 );
     list.push_back( 1 );
     list.push_back( 2 );
@@ -130,15 +130,15 @@ TEST( list, erase_midle )
 
     // Assert
     ASSERT_EQ( list.size(), 2 );
-    std::list<int> list_expect = {0, 2};
+    std::forward_list<int> list_expect = {0, 2};
     ASSERT_TRUE( std::equal( list_expect.begin(), list_expect.end(), list.begin() ) );
 }
 
-TEST( list, get_value)
+TEST( forward_list, get_value)
 {
     // Arrange
     const int count = 10;
-    MyList<int> list;
+    MyForwardList<int> list;
 
     // Act
     for( int i = 0; i < count; ++i )
@@ -154,14 +154,14 @@ TEST( list, get_value)
     }
 }
 
-TEST( list, copy )
+TEST( forward_list, copy )
 {
     // Arrange
-    MyList<int> list;
+    MyForwardList<int> list;
     list.push_back( 10 );
     list.push_back( 20 );
     list.push_back( 30 );
-    MyList<int> list_copy;
+    MyForwardList<int> list_copy;
 
     // Act
     list_copy = list;
--- 06.homework/test/vector_tests.cpp
+++ 06.homework/test/vector_tests.cpp
@@ -31,4 +31,141 @@ TEST( vector, push_back )
 
     // Assert
     ASSERT_EQ( vector.size(), count );
+    std::vector<int> vector_expect = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    ASSERT_TRUE( std::equal( vector_expect.begin(), vector_expect.end(), vector.begin() ) );
+}
+
+TEST( vector, insert_back )
+{
+    // Arrange
+    MyContainer<int> vector;
+    vector.push_back( 0 );
+    vector.push_back( 1 );
+
+    // Act
+    vector.insert( 2, 2 );
+
+    // Assert
+    ASSERT_EQ( vector.size(), 3 );
+    std::vector<int> vector_expect = {0, 1 , 2};
+    ASSERT_TRUE( std::equal( vector_expect.begin(), vector_expect.end(), vector.begin() ) );
+}
+
+TEST( vector, insert_front )
+{
+    // Arrange
+    MyContainer<int> vector;
+    vector.push_back( 0 );
+    vector.push_back( 1 );
+
+    // Act
+    vector.insert( 0, -1 );
+
+    // Assert
+    ASSERT_EQ( vector.size(), 3 );
+    std::vector<int> vector_expect = {-1, 0 , 1};
+    ASSERT_TRUE( std::equal( vector_expect.begin(), vector_expect.end(), vector.begin() ) );
+}
+
+TEST( vector, insert_midle )
+{
+    // Arrange
+    MyContainer<int> vector;
+    vector.push_back( 0 );
+    vector.push_back( 1 );
+
+    // Act
+    vector.insert( 1, 2 );
+
+    // Assert
+    ASSERT_EQ( vector.size(), 3 );
+    std::vector<int> vector_expect = {0, 2 , 1};
+    ASSERT_TRUE( std::equal( vector_expect.begin(), vector_expect.end(), vector.begin() ) );
+}
+
+TEST( vector, erase_back )
+{
+    // Arrange
+    MyContainer<int> vector;
+    vector.push_back( 0 );
+    vector.push_back( 1 );
+    vector.push_back( 2 );
+
+    // Act
+    vector.erase( 2 );
+
+    // Assert
+    ASSERT_EQ( vector.size(), 2 );
+    std::vector<int> vector_expect = {0, 1};
+    ASSERT_TRUE( std::equal( vector_expect.begin(), vector_expect.end(), vector.begin() ) );    
+}
+
+TEST( vector, erase_front )
+{
+    // Arrange
+    MyContainer<int> vector;
+    vector.push_back( 0 );
+    vector.push_back( 1 );
+    vector.push_back( 2 );
+
+    // Act
+    vector.erase( 0 );
+
+    // Assert
+    ASSERT_EQ( vector.size(), 2 );
+    std::vector<int> vector_expect = {1, 2};
+    ASSERT_TRUE( std::equal( vector_expect.begin(), vector_expect.end(), vector.begin() ) );    
+}
+
+TEST( vector, erase_midle )
+{
+    // Arrange
+    MyContainer<int> vector;
+    vector.push_back( 0 );
+    vector.push_back( 1 );
+    vector.push_back( 2 );
+
+    // Act
+    vector.erase( 1 );
+
+    // Assert
+    ASSERT_EQ( vector.size(), 2 );
+    std::vector<int> vector_expect = {0, 2};
+    ASSERT_TRUE( std::equal( vector_expect.begin(), vector_expect.end(), vector.begin() ) );    
+}
+
+TEST( vector, get_value)
+{
+    // Arrange
+    const int count = 10;
+    MyContainer<int> vector;
+
+    // Act
+    for( int i = 0; i < count; ++i )
+    {
+        vector.push_back( i );
+    }
+
+    // Assert
+    ASSERT_EQ( vector.size(), count );
+    for( int i = 0; i < count; ++i )
+    {
+        ASSERT_EQ( vector[i], i );
+    }
+}
+
+TEST( vector, copy )
+{
+    // Arrange
+    MyContainer<int> vector;
+    vector.push_back( 10 );
+    vector.push_back( 20 );
+    vector.push_back( 30 );
+    MyContainer<int> vector_copy;
+
+    // Act
+    vector_copy = vector;
+
+    // Assert
+    ASSERT_TRUE( std::equal( vector.begin(), vector.end(), vector_copy.begin() ) );
 }
