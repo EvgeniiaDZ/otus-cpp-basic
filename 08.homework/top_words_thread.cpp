5a32613291017a2e21fe02002a7f994368dd7f64
EvgeniiaD <zhen_da@mail.ru>
2022-08-07 13:46:34 +0700

    Add initial files.

---
+++ 08.homework/top_words_thread.cpp
@@ -0,0 +1,98 @@
+// Read files and prints top k word by frequency
+
+#include <algorithm>
+#include <cctype>
+#include <cstdlib>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <vector>
+#include <chrono>
+#include <thread>
+
+const size_t TOPK = 10;
+
+using Counter = std::map<std::string, std::size_t>;
+
+std::string tolower(const std::string &str);
+
+void count_words(std::istream& stream, Counter&);
+
+void print_topk(std::ostream& stream, const Counter&, const size_t k);
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        std::cerr << "Usage: topk_words [FILES...]\n";
+        return EXIT_FAILURE;
+    }
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    std::vector<std::thread> thrs;
+    int size = argc - 1;
+    std::vector<std::ifstream> input(size);
+    std::vector<Counter> freq_dict(size);
+    
+    for (int i = 1; i < argc; ++i) {
+        input[i - 1].open(argv[i]);
+        if (!input[i - 1].is_open()) {
+            std::cerr << "Failed to open file " << argv[i] << '\n';
+            for (auto &thr_item : thrs) {
+                thr_item.join();
+            }
+            return EXIT_FAILURE;
+        }
+        thrs.emplace_back(count_words, std::ref(input[i - 1]), std::ref(freq_dict[i - 1]));
+    }
+
+    for (auto &thr_item : thrs) {
+        thr_item.join();
+    }
+
+    Counter freq_dict_sum;
+    for (int i = 0; i < size; ++i) {
+        for (auto item : freq_dict[i]) {
+            freq_dict_sum[item.first] += item.second;
+        }
+    }
+    
+    print_topk(std::cout, freq_dict_sum, TOPK);
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed_ms = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+    std::cout << "Elapsed time is " << elapsed_ms.count() << " us\n";
+}
+
+std::string tolower(const std::string &str) {
+    std::string lower_str;
+    std::transform(std::cbegin(str), std::cend(str),
+                   std::back_inserter(lower_str),
+                   [](unsigned char ch) { return std::tolower(ch); });
+    return lower_str;
+}
+
+void count_words(std::istream& stream, Counter& counter) {
+    std::for_each(std::istream_iterator<std::string>(stream),
+                  std::istream_iterator<std::string>(),
+                  [&counter](const std::string &s) { ++counter[tolower(s)]; });    
+}
+
+void print_topk(std::ostream& stream, const Counter& counter, const size_t k) {
+    std::vector<Counter::const_iterator> words;
+    words.reserve(counter.size());
+    for (auto it = std::cbegin(counter); it != std::cend(counter); ++it) {
+        words.push_back(it);
+    }
+
+    std::partial_sort(
+        std::begin(words), std::begin(words) + k, std::end(words),
+        [](auto lhs, auto &rhs) { return lhs->second > rhs->second; });
+
+    std::for_each(
+        std::begin(words), std::begin(words) + k,
+        [&stream](const Counter::const_iterator &pair) {
+            stream << std::setw(4) << pair->second << " " << pair->first
+                      << '\n';
+        });
+}
